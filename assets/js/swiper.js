78851aa68f41c1f5543df641b92d36ecabe454b7
alicanov98 <alicanov1998@gmail.com>
2023-04-22 00:40:57 +0400

    edit

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,6 +1,11 @@
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
   spaceBetween: 30,
+  autoplay: {
+    delay: 1000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
   loop: true,
   breakpoints: {
     250: {
@@ -9,6 +14,35 @@ var swiper = new Swiper(".mySwiper", {
     },
     768: {
       slidesPerView: 2,
+      spaceBetween: 30,
+    },
+  },
+});
+
+var cilentSwiper = new Swiper(".clientSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 1000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+  loop: true,
+  breakpoints: {
+    250: {
+      slidesPerView: 1,
+      spaceBetween: 30,
+    },
+    580: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+    992: {
+      slidesPerView: 5,
       spaceBetween: 30,
     },
   },
